257c422cb1bb9b4bf09999b38b1730f97bccb92b
ChandraMohan-Sah <csah9628@gmail.com>
2025-07-01 13:12:07 +0545

    re-ran migrations command

---
+++ apps/user_app/tests.py
@@ -0,0 +1,77 @@
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase 
+from rest_framework.authtoken.models import Token 
+
+
+
+class TestRegisterationView(APITestCase):
+
+    def test_register(self):
+        url = reverse('registration_view')
+        data = {
+            "username" : "testcase",
+            "email" : "testcase@example.com",
+            "password" : "NewPassword@123",
+            "confirm_password" : "NewPassword@123"
+        }
+
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+
+
+
+
+'''
+I used APITestCase for testing the API endpoints.
+All the necessary methds that it contains are :
+    1. self.client.get()
+    2. self.client.post()
+    3. self.client.put()
+    4. self.client.patch()
+    5. self.client.delete()
+
+    6. self.client.head()
+    7. self.client.options()
+    8. self.client.trace()
+
+    9. self.client.login()
+    10. self.client.logout()
+    11. self.client.force_authenticate()
+    12. self.client.credentials()
+
+    13. self.client.request()
+    14. self.client.generic()
+
+    15. self.client.assertContains()
+    16. self.client.assertNotContains()
+
+    17. self.client.assertRedirects()
+
+    20. self.client.assertFormError()
+    21. self.client.assertFormSetError()
+    22. self.client.assertQuerysetEqual()
+    23. self.client.assertNumQueries()
+    24. self.client.assertJSONEqual()
+    25. self.client.assertJSONNotEqual()
+
+    30. self.client.assertURLEqual()
+    31. self.client.assertURINotEqual()
+
+    32. self.assertEqual()
+    33. self.assertNotEqual()
+    34. self.assertTrue()
+    35. self.assertFalse()
+    36. self.assertIs()
+    37. self.assertIsNot()
+    38. self.assertIsNone()
+    39. self.assertIsNotNone()
+    40. self.assertIn()
+    41. self.assertNotIn()
+    42. self.assertIsInstance()
+    43. self.assertNotIsInstance()
+
+'''
+
