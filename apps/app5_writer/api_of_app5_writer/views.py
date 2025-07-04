cfe69de066252e2c2e015d834674505bb717b04e
ChandraMohan-Sah <csah9628@gmail.com>
2025-07-04 17:12:46 +0545

    feat: added pagination, permissions on app5_creator

--- apps/app5_writer/api_of_app5_writer/views.py
+++ apps/app5_writer/api_of_app5_writer/views.py
@@ -11,68 +11,82 @@ from rest_framework import generics
 from drf_yasg.utils import swagger_auto_schema 
 from django.utils.decorators import method_decorator 
 
+#pagination 
+from rest_framework import pagination
+from shared.pagination import GlobalPagination
+
+
+#permissions
+from rest_framework import permissions
+from shared.permissions import IsAdminOrReadOnly
+
 
 @method_decorator(name='get', decorator=swagger_auto_schema(
-    tags=['App5 : Writer APIs'], operation_id='list all  writer',
-    operation_description='list all writer',
+    tags=['App5 : Writer APIs'], operation_id='list all  writer [IsAdminOrReadOnly] [paginate-10]',
+    operation_description='list all writer [IsAdminOrReadOnly]',
 ))
 @method_decorator(name='post', decorator=swagger_auto_schema(
-    tags=['App5 : Writer APIs'], operation_id='create a writer',
-    operation_description='create a writer',
+    tags=['App5 : Writer APIs'], operation_id='create a writer [IsAdminOrReadOnly]',
+    operation_description='create a writer [IsAdminOrReadOnly]',
 )) 
 class Writer_ListCreate_View(generics.ListCreateAPIView):
     queryset = Writer.objects.all().select_related('writermedia').prefetch_related('writermedia__media_files')
     serializer_class = WriterSerializer 
+    permission_classes = [IsAdminOrReadOnly]
+    pagination_class = [GlobalPagination]
 
 
 @method_decorator(name='get', decorator=swagger_auto_schema(
-    tags=['App5 : Writer APIs'], operation_id='retrieve particular writer detail',
+    tags=['App5 : Writer APIs'], operation_id='retrieve particular writer detail [IsAdminOrReadOnly]',
 ))
 @method_decorator(name='put', decorator=swagger_auto_schema(
-    tags=['App5 : Writer APIs'], operation_id='update particular writer detail',
+    tags=['App5 : Writer APIs'], operation_id='update particular writer detail [IsAdminOrReadOnly]',
 ))
 @method_decorator(name='patch', decorator=swagger_auto_schema(
-    tags=['App5 : Writer APIs'], operation_id='patch particular writer detail',
+    tags=['App5 : Writer APIs'], operation_id='patch particular writer detail [IsAdminOrReadOnly]',
 ))
 @method_decorator(name='delete', decorator=swagger_auto_schema(
-    tags=['App5 : Writer APIs'], operation_id='delete particular writer detail',
+    tags=['App5 : Writer APIs'], operation_id='delete particular writer detail [IsAdminOrReadOnly]',
 ))
 class Writer_RUD_View(generics.RetrieveUpdateDestroyAPIView):
     queryset = Writer.objects.all().select_related('writermedia').prefetch_related('writermedia__media_files')
     serializer_class = WriterSerializer 
+    permission_classes = [IsAdminOrReadOnly]
  
 
 
 
 
 @method_decorator(name='get', decorator=swagger_auto_schema(
-    tags=['App5 : WriterCoreDetail APIs'], operation_id='list all  writer core detail',
-    operation_description='list all writer core detail',
+    tags=['App5 : WriterCoreDetail APIs'], operation_id='list all  writer core detail [IsAdminOrReadOnly] [paginate-10]',
+    operation_description='list all writer core detail [IsAdminOrReadOnly]',
 ))
 @method_decorator(name='post', decorator=swagger_auto_schema(
-    tags=['App5 : WriterCoreDetail APIs'], operation_id='create a writer core detail',
-    operation_description='create a writer core detail',
+    tags=['App5 : WriterCoreDetail APIs'], operation_id='create a writer core detail [IsAdminOrReadOnly]',
+    operation_description='create a writer core detail [IsAdminOrReadOnly]',
 )) 
 class WriterCoreDetail_ListCreate_View(generics.ListCreateAPIView):
     queryset = WriterCoreDetail.objects.select_related('writer')
     serializer_class = WriterCoreDetailSerializer 
+    permission_classes = [IsAdminOrReadOnly]
 
 
 
 @method_decorator(name='get', decorator=swagger_auto_schema(
-    tags=['App5 : WriterCoreDetail APIs'], operation_id='retrieve particular writer core detail detail',
+    tags=['App5 : WriterCoreDetail APIs'], operation_id='retrieve particular writer core detail detail [IsAdminOrReadOnly]',
 ))
 @method_decorator(name='put', decorator=swagger_auto_schema(
-    tags=['App5 : WriterCoreDetail APIs'], operation_id='update particular writer core derial detail',
+    tags=['App5 : WriterCoreDetail APIs'], operation_id='update particular writer core derial detail [IsAdminOrReadOnly]',
 ))
 @method_decorator(name='patch', decorator=swagger_auto_schema(
-    tags=['App5 : WriterCoreDetail APIs'], operation_id='patch particular writer core detail detail',
+    tags=['App5 : WriterCoreDetail APIs'], operation_id='patch particular writer core detail detail [IsAdminOrReadOnly]',
 ))
 @method_decorator(name='delete', decorator=swagger_auto_schema(
-    tags=['App5 : WriterCoreDetail APIs'], operation_id='delete particular writer core detail detail',
+    tags=['App5 : WriterCoreDetail APIs'], operation_id='delete particular writer core detail detail [IsAdminOrReadOnly]',
 ))
 class WriterCoreDetail_RUD_View(generics.RetrieveUpdateDestroyAPIView):
     queryset = WriterCoreDetail.objects.all().select_related('writer')
     serializer_class = WriterCoreDetailSerializer
+    permission_classes = [IsAdminOrReadOnly]
 
 
