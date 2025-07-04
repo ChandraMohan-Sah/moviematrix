49a2dcdca79ef1df14da42edb0f24a0d8240e8fb
ChandraMohan-Sah <csah9628@gmail.com>
2025-07-04 17:03:19 +0545

    feat: added permissions, pagination in app3_cast

--- apps/app3_cast/api_of_app3_cast/views.py
+++ apps/app3_cast/api_of_app3_cast/views.py
@@ -10,65 +10,79 @@ from rest_framework import generics
 from drf_yasg.utils import swagger_auto_schema
 from django.utils.decorators import method_decorator
 
+#pagination 
+from rest_framework import pagination
+from shared.pagination import GlobalPagination
+
+
+#permissions
+from rest_framework import permissions
+from shared.permissions import IsAdminOrReadOnly
 
 @method_decorator(name='get', decorator=swagger_auto_schema(
-    tags=['App3 : Cast APIs'], operation_id='list all  cast',
+    tags=['App3 : Cast APIs'], operation_id='list all  cast [IsAdminOrReadOnly] [Paginate-10]',
     operation_description='list all cast',
 ))
 @method_decorator(name='post', decorator=swagger_auto_schema(
-    tags=['App3 : Cast APIs'], operation_id='create a cast',
+    tags=['App3 : Cast APIs'], operation_id='create a cast [IsAdminOrReadOnly]',
     operation_description='create a cast',
 ))
 class Cast_ListCreate_View(generics.ListCreateAPIView):
     queryset = Cast.objects.all().select_related('castmedia').prefetch_related('castmedia__media_files')
     serializer_class = CastSerializer
+    permission_classes = [IsAdminOrReadOnly]
+    pagination_class = GlobalPagination
 
 
  
 @method_decorator(name='get', decorator=swagger_auto_schema(
-    tags=['App3 : Cast APIs'], operation_id='retrieve particular cast detail',
+    tags=['App3 : Cast APIs'], operation_id='retrieve particular cast detail [IsAdminOrReadOnly]',
 ))
 @method_decorator(name='put', decorator=swagger_auto_schema(
-    tags=['App3 : Cast APIs'], operation_id='update particular cast detail',
+    tags=['App3 : Cast APIs'], operation_id='update particular cast detail [IsAdminOrReadOnly]',
 ))
 @method_decorator(name='patch', decorator=swagger_auto_schema(
-    tags=['App3 : Cast APIs'], operation_id='patch particular cast detail',
+    tags=['App3 : Cast APIs'], operation_id='patch particular cast detail [IsAdminOrReadOnly]',
 ))
 @method_decorator(name='delete', decorator=swagger_auto_schema(
-    tags=['App3 : Cast APIs'], operation_id='delete particular cast detail',
+    tags=['App3 : Cast APIs'], operation_id='delete particular cast detail [IsAdminOrReadOnly]',
 ))
 class Cast_RUD_View(generics.RetrieveUpdateDestroyAPIView):
     queryset = Cast.objects.all().select_related('castmedia').prefetch_related('castmedia__media_files')
     serializer_class = CastSerializer
+    permission_classes = [IsAdminOrReadOnly]
 
 
 
 
 @method_decorator(name='get', decorator=swagger_auto_schema(
-    tags=['App3 : CastCoreDetail APIs'], operation_id='list all core detail of  cast',
-    operation_description='list all core detail of  cast',
+    tags=['App3 : CastCoreDetail APIs'], operation_id='list all core detail of  cast [IsAdminOrReadOnly] [Paginate-10]',
+    operation_description='list all core detail of  cast [IsAdminOrReadOnly]',
 ))
 @method_decorator(name='post', decorator=swagger_auto_schema(
-    tags=['App3 : CastCoreDetail APIs'], operation_id='create a core detail of cast',
-    operation_description='create a core detail of cast',
+    tags=['App3 : CastCoreDetail APIs'], operation_id='create a core detail of cast [IsAdminOrReadOnly]',
+    operation_description='create a core detail of cast [IsAdminOrReadOnly]',
 ))
 class CastCoreDetail_ListCreate_View(generics.ListCreateAPIView):
     queryset = CastCoreDetail.objects.select_related('cast')
     serializer_class = CastCoreDetailSerializer
+    permission_classes = [IsAdminOrReadOnly]
+    pagination_class = GlobalPagination
 
 
 @method_decorator(name='get', decorator=swagger_auto_schema(
-    tags=['App3 : CastCoreDetail APIs'], operation_id='retrieve particular cast core detail',
+    tags=['App3 : CastCoreDetail APIs'], operation_id='retrieve particular cast core detail [IsAdminOrReadOnly]',
 ))
 @method_decorator(name='put', decorator=swagger_auto_schema(
-    tags=['App3 : CastCoreDetail APIs'], operation_id='update particular cast core detail',
+    tags=['App3 : CastCoreDetail APIs'], operation_id='update particular cast core detail [IsAdminOrReadOnly]',
 ))
 @method_decorator(name='patch', decorator=swagger_auto_schema(
-    tags=['App3 : CastCoreDetail APIs'], operation_id='patch particular cast core detail',
+    tags=['App3 : CastCoreDetail APIs'], operation_id='patch particular cast core detail [IsAdminOrReadOnly]',
 ))
 @method_decorator(name='delete', decorator=swagger_auto_schema(
-    tags=['App3 : CastCoreDetail APIs'], operation_id='delete particular cast core detail',
+    tags=['App3 : CastCoreDetail APIs'], operation_id='delete particular cast core detail [IsAdminOrReadOnly]',
 ))
 class CastCoreDetail_RUD_View(generics.RetrieveUpdateDestroyAPIView):
     queryset = CastCoreDetail.objects.select_related('cast')
     serializer_class = CastCoreDetailSerializer
+    permission_classes = [IsAdminOrReadOnly]
